b46b202a89cbe6138594a55389a86bd3e1d46714
rosalieacco <rosalieacco@gmail.com>
2025-02-24 17:23:32 +0100

    Modification de two.tsx

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,15 +1,23 @@
-import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Button } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
+  const [message, setMessage] = useState('This is tab two');
+
+  const handlePress = () => {
+    setMessage('Le texte a changé ! 🎉');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>{message}</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <Button title="Clique ici" onPress={handlePress} />
+        <EditScreenInfo path="app/(tabs)/two.tsx" />
+      </View>
   );
 }
 
@@ -20,8 +28,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
+    color: '#4760ff', // Couleur personnalisée
+    marginBottom: 20,
   },
   separator: {
     marginVertical: 30,
