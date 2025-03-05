affd708943e44cf35ef32a8f18e84a5a30957fda
huwil <huwillien56@gmail.com>
2025-03-05 16:35:22 +0100

    Ajout image new assos

--- config.tsx
+++ config.tsx
@@ -1,6 +1,14 @@
-export const BASE_URL = 'http://192.168.1.134:3000'; //à changer
+export const BASE_URL = 'http://192.168.166.220:3000'; //à changer
 export const images = {
     "AEJS.png": require("@/assets/images/AEJS.png"),
     "ENDOFRANCE.png": require("@/assets/images/ENDOFRANCE.png"),
+    "ASFC-logo.png": require("@/assets/images/ASFC-logo.png"),
+    "AMI-logo.png": require("@/assets/images/AMI-logo.png"),
+    "FFDSB-logo.jpg": require("@/assets/images/FFDSB-logo.jpg"),
+    "ENTRAIDADDICT-logo.png": require("@/assets/images/ENTRAIDADDICT-logo.png"),
+    "ASBH-logo.jpg": require("@/assets/images/ASBH-logo.jpg"),
+    "RES-logo.jpg": require("@/assets/images/RES-logo.jpg"),
+    "ARSLA-logo.jpg": require("@/assets/images/ARSLA-logo.jpg"),
+    "AFS-logo.jpg": require("@/assets/images/AFS-logo.jpg"),
 };
 
