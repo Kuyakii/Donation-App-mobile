2d22367db853330cf25d29f015694de13a0add80
huwil <huwillien56@gmail.com>
2025-03-12 13:05:53 +0100

    fix code j'ai oublié ça (shadow ne fonctionne pas de mon côté)

--- components/BoutonAccueil.tsx
+++ components/BoutonAccueil.tsx
@@ -1,39 +1,43 @@
 import React from 'react';
-import {View, TouchableOpacity, StyleSheet, Text} from 'react-native';
-import {router} from "expo-router";
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { router } from "expo-router";
+import Colors from "@/constants/Colors";
 
-const BoutonAccueil= () => {
-    const redirect =  () => {
-            router.replace('/(tabs)');
+const BoutonAccueil = () => {
+    const navigation = useNavigation();
+    const redirect = () => {
+        // @ts-ignore
+        navigation.navigate('(tabs)', {
+            screen: 'index',
+        });
     };
 
     return (
         <View>
             <TouchableOpacity style={styles.button} onPress={redirect}>
-                <Text style={styles.buttonText} >Retour</Text>
+                <Text style={styles.buttonText}>‹</Text>
             </TouchableOpacity>
         </View>
     );
 };
+
 const styles = StyleSheet.create({
     button: {
-        height: 40,
-        width: 100,
-        backgroundColor: '#2563EB',
-        borderRadius: 12,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 30,
+        height: 60,
+        width: 30,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 4 },
         shadowOpacity: 0.6,
         shadowRadius: 5,
-        elevation: 3
+        elevation: 3,
     },
     buttonText: {
-        color: '#FFF',
-        fontSize: 16,
+        color: Colors.primary_dark.background,
+        fontSize: 45,
         fontWeight: 'bold',
+        textAlign: 'center', // Assure le centrage du texte
     },
 });
+
 export default BoutonAccueil;
